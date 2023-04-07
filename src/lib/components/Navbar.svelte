d71978e37b9d16e06c351a80afcbfe6ea1b925c8
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-04-07 14:21:08 +0200

    Chore: made navigationsOption reusable

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -8,12 +8,19 @@
 	function toggleMenu() {
 		isMenuOpen = !isMenuOpen;
 	}
+
+	const navigationOptions = [
+		{ title: 'About', link: '/about' },
+		{ title: 'Contact', link: '/contact' },
+		{ title: 'Resources', link: '/resources' },
+		{ title: 'Join us', link: '/join-us' },
+	];
 </script>
 
 <nav class="flex h-20 bg-gray-800">
 	<div class="ml-4 flex flex-1 items-center">
 		<button
-			class="mr-4 text-2xl font-bold focus:outline-none"
+			class="mr-4 text-2xl font-bold focus:outline-none md:hidden"
 			on:click={toggleMenu}
 		>
 			<span
@@ -35,36 +42,36 @@
 				class:-translate-y-0={!isMenuOpen}
 			/>
 		</button>
+		<div class="hidden md:flex md:items-center">
+			{#each navigationOptions as option (option.title)}
+				<a href={option.link} class="ml-4 text-white">{option.title}</a>
+			{/each}
+		</div>
 	</div>
 	<div class="flex flex-1 items-center justify-center" />
 	<div class="mr-4 flex flex-1 items-center justify-end">
 		{#if session}
-			{#if session.user?.image}
-				<span
-					style="background-image: url('{session.user.image}')"
-					class="avatar"
-				/>
-			{/if}
-			<span class="signedInText">
+			<span class="userName">
 				<strong>{session.user?.name}</strong>
 			</span>
 			<button on:click={() => signOut()} class="button">Sign out</button>
 		{:else}
-			<button on:click={() => signIn()}>Sign In</button>
+			<button on:click={() => signIn()} class="button">Sign In</button>
 		{/if}
 	</div>
 </nav>
 {#if isMenuOpen}
 	<div
-		class="menu-transition fixed left-0 top-20 h-full w-64 transform bg-gray-800 p-4 text-white transition duration-500 ease-in-out"
+		class="menu-transition fixed left-0 top-16 h-full w-32 transform bg-gray-800 p-4 text-white transition duration-500 ease-in-out"
 		class:translate-x-0={isMenuOpen}
 		class:translate-x-full={!isMenuOpen}
 	>
 		<ul class="space-y-2">
-			<li><a class="hover:text-gray-400" href="#">About</a></li>
-			<li><a class="hover:text-gray-400" href="#">Resources</a></li>
-			<li><a class="hover:text-gray-400" href="#">Contact</a></li>
-			<li><a class="hover:text-gray-400" href="#">Join us</a></li>
+			{#each navigationOptions as option (option.title)}
+				<li>
+					<a class="hover:text-gray-400" href={option.link}>{option.title}</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 {/if}
