726e8b521ea378545229300bd8b28a87c12ffcaf
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-04-07 21:47:49 +0200

    Bug: Adding missing files for service-worker/manifest

--- src/lib/db/schema.d.ts
+++ src/lib/db/schema.d.ts
@@ -4,11 +4,51 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 
+export interface Account {
+  id: string;
+  userId: string;
+  type: string;
+  provider: string;
+  providerAccountId: string;
+  refreshToken: string | null;
+  accessToken: string | null;
+  expiresAt: number | null;
+  tokenType: string | null;
+  scope: string | null;
+  idToken: string | null;
+  sessionState: string | null;
+}
+
 export interface Example {
   id: Generated<number>;
   name: string;
 }
 
+export interface Session {
+  id: string;
+  sessionToken: string;
+  userId: string;
+  expires: Date;
+}
+
+export interface User {
+  id: string;
+  name: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+}
+
+export interface VerificationToken {
+  identifier: string;
+  token: string;
+  expires: Date;
+}
+
 export interface DB {
+  Account: Account;
   Example: Example;
+  Session: Session;
+  User: User;
+  VerificationToken: VerificationToken;
 }
