703a9f4d638702510aa1e7061e6bb57244a4c5c0
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-04-07 22:26:53 +0200

    Bug: Completely removing service-worker temporarily

--- src/service-worker.ts
+++
@@ -1,6 +0,0 @@
-/// <reference lib="webworker" />
-
-import { build, files, version } from '$service-worker';
-
-const worker = self as unknown as ServiceWorkerGlobalScope;
-const FILES = `cache${version}`;
