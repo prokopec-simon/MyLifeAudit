0675885e15808b7e9b987ad05ae986e55e8ff79f
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-04-08 09:47:05 +0200

    Bug: Trying to include Prisma auth adapter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,15 +3,11 @@ import GitHub from '@auth/core/providers/github';
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
 import type { Provider } from '@auth/core/providers';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import type { Adapter } from '@auth/core/adapters';
 import { prisma } from './lib/server/prisma';
 
-// const prisma = new PrismaClient();
-
 export const handle = SvelteKitAuth({
-	//adapter: PrismaAdapter(prisma) as Adapter,
-
+	adapter: PrismaAdapter(prisma) as Adapter,
 	session: {
 		strategy: 'database',
 		generateSessionToken: () => {
