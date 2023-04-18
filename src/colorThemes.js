fab578fcbe8c62644107339ecb389168d145b890
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-04-18 09:34:56 +0200

    Chore: Linting

--- src/colorThemes.js
+++ src/colorThemes.js
@@ -3,12 +3,12 @@ export const colors = {
 		primary: '#F6F4EE',
 		secondary: 'darkblue',
 		brand: '#3AA89B',
-    text_primary: '#000000'
+		text_primary: '#000000',
 	},
 	dark: {
 		primary: '#2B2826',
 		secondary: '#E9C46A',
 		brand: '#A3D2CA',
-    text_primary: '#FFFFFF'
+		text_primary: '#FFFFFF',
 	},
 };
