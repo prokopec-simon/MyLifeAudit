0a5d9f50bb41a5f76a51d5a84f11cf696a123e64
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-05-24 11:49:11 +0200

    updated color themes

--- src/colorThemes.js
+++ src/colorThemes.js
@@ -1,13 +1,11 @@
 export const colors = {
 	light: {
 		primary: '#F6F4EE',
-		secondary: 'darkblue',
 		brand: '#3AA89B',
 		text_primary: '#000000',
 	},
 	dark: {
-		primary: '#2B2826',
-		secondary: '#E9C46A',
+		primary: '#012627',
 		brand: '#3AA89B',
 		text_primary: '#FFFFFF',
 	},
