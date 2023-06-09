47ba10b5be22e09754d73463d2df0be1147ebbdb
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-06-09 15:12:15 +0200

    Feat: range picking UI rework, not finished

--- src/lib/components/RangePicker.svelte
+++ src/lib/components/RangePicker.svelte
@@ -12,6 +12,26 @@
 
 		window.dispatchEvent(event);
 	};
+
+	const hoverStyles = {
+		0: 'hover:bg-green-600',
+		1: 'hover:bg-green-400',
+		2: 'hover:bg-green-200',
+		3: 'hover:bg-gray-100',
+		4: 'hover:bg-red-200',
+		5: 'hover:bg-red-400',
+		6: 'hover:bg-red-600',
+	};
+
+	const colors = {
+		0: 'bg-green-600',
+		1: 'bg-green-400',
+		2: 'bg-green-200',
+		3: 'bg-gray-100',
+		4: 'bg-red-200',
+		5: 'bg-red-400',
+		6: 'bg-red-600',
+	};
 </script>
 
 <div class="grid grid-cols-4 justify-center">
@@ -23,15 +43,21 @@
 	<div class="col-span-4 md:col-span-2">
 		{#each Array(7) as _, i}
 			<button
-				class="m-1 h-6 w-6 cursor-pointer rounded-lg bg-gray-300 md:h-8 md:w-8 {i +
-					1 ===
-				selectedValue
-					? 'bg-gray-800'
-					: ''}"
+				class="m-1 h-6 w-6 cursor-pointer rounded-lg border border-solid border-brand_brick md:h-8 md:w-8"
 				on:click={() => handleDotClick(i + 1)}
-			/>
+			>
+				<div class="relative h-full w-11/12">
+					<div
+						class="absolute inset-0 m-1 rounded-full bg-white opacity-0 hover:opacity-100 {selectedValue ===
+						i + 1
+							? 'opacity-100'
+							: ''} {colors[i]}"
+					/>
+				</div>
+			</button>
 		{/each}
 	</div>
+
 	<div
 		class="col-start-4 row-start-2 mt-2 text-lg md:col-start-4 md:row-start-1 md:ml-4"
 	>
